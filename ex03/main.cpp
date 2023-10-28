e47a392133d0f72a54c94e29d5abf146b61c0344
unknown <jamie.gworld@hotmail.com>
2023-10-28 21:55:39 +0700

    OK

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -1,4 +1,5 @@
 #include "Bureaucrat.hpp"
+#include "AForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
 #include "ShrubberyCreationForm.hpp"
@@ -6,24 +7,44 @@
 
 int	main()
 {
+	Form *form1 = 0;
+	Form *form2 = 0;
+	Form *form3 = 0;
+	try
 	{
-		try
+		Bureaucrat bureaucrat("Me", 2);
+		Intern intern;
+		form1 = intern.makeForm("ShrubberyCreationForm", "home");
+		if (form1)
 		{
-			Intern someRandomIntern;
-			Form* rrf;
-			rrf = someRandomIntern.makeForm("ShrubberyCreationForm", "Bender");
-			delete rrf;
-			rrf = someRandomIntern.makeForm("PresidentialPardonForm", "Bender");
-			delete rrf;
-			rrf = someRandomIntern.makeForm("RobotomyRequestForm", "Bender");
-			delete rrf;
-			rrf = someRandomIntern.makeForm("testdsadwqdqwdqwdqwdqww", "Bender");
-			delete rrf;
+			bureaucrat.signForm(*form1);
+			bureaucrat.executeForm(*form1);
+			delete form1;
+			form1 = 0;
 		}
-		catch (Intern::InvalidForm &e)
+		form2 = intern.makeForm("RobotomyRequestForm", "home");
+		if (form2)
 		{
-			std::cout << e.what() << std::endl;
+			bureaucrat.signForm(*form2);
+			bureaucrat.executeForm(*form2);
+			bureaucrat.executeForm(*form2);
+			bureaucrat.executeForm(*form2);
+			bureaucrat.executeForm(*form2);
+			delete form2;
+			form2 = 0;
 		}
+		form3 = intern.makeForm("PresidentialPardonForm", "home");
+		if (form3)
+		{
+			bureaucrat.signForm(*form3);
+			bureaucrat.executeForm(*form3);
+			delete form3;
+			form3 = 0;
+		}
+	}
+	catch (Intern::InvalidForm &e)
+	{
+		std::cout << e.what() << std::endl;
 	}
 	return (0);
 }
